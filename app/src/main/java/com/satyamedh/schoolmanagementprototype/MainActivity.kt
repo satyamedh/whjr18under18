4451a7f998a160149a2518f71730241fb7aff50f
satyamedh hulyalkar <satyamedh9@gmail.com>
2021-03-15 21:00:12 +0530

    Upgraded Login

--- app/src/main/java/com/satyamedh/schoolmanagementprototype/MainActivity.kt
+++ app/src/main/java/com/satyamedh/schoolmanagementprototype/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.satyamedh.schoolmanagementprototype
 
 import android.annotation.SuppressLint
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -14,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,27 +36,44 @@ class MainActivity : AppCompatActivity() {
         val passwordBox = findViewById<EditText>(R.id.editTextTextPassword)
         val email = emailBox.text
         val password = passwordBox.text
-        loginButton.setOnClickListener { login(email.toString(), password.toString()) }
+
+        progressBar = ProgressDialog(this)
+        progressBar.setCancelable(false)
+
+        progressBar.setMessage("Logging in")
+        progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+        progressBar.setProgressPercentFormat(null)
+        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+
+        loginButton.setOnClickListener { login(emailBox, passwordBox) }
 
     }
 
-    private fun login(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        if (user == null) {
-                            Toast.makeText(this, "Login failed. try again", Toast.LENGTH_LONG).show()
+    private fun login(emailBox: EditText, passwordBox: EditText) {
+        val email = emailBox.text
+        val password = passwordBox.text
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill in all the details", Toast.LENGTH_LONG).show()
+            return
+        }else {
+            progressBar.show()
+            auth.signInWithEmailAndPassword(email.toString(), password.toString())
+                    .addOnCompleteListener(this) { task ->
+                        progressBar.hide()
+                        if (task.isSuccessful) {
+                            val user = auth.currentUser
+                            if (user == null) {
+                                Toast.makeText(this, "Login failed. try again", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(this, "LOGIN SUCCESS", Toast.LENGTH_LONG).show()
+                            }
                         } else {
-                            Toast.makeText(this, "LOGIN SUCCESS", Toast.LENGTH_LONG).show()
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
                         }
-                    } else {
-                        Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
                     }
-                }
 
-
+        }
     }
 
 
